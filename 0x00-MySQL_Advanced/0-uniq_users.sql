40c4df4d2b45ef321dc78a9d96527475713d960e
IamSila <silamulingi@gmail.com>
2024-11-13 16:46:00 +0300

    added the directory, readme, and first task's file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- creates a table users
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+);
