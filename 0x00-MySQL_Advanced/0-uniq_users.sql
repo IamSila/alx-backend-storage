9ba085e5b37f4bf4586e941c8bdccf4766975e0f
IamSila <silamulingi@gmail.com>
2024-11-13 16:49:23 +0300

    sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- creates a table users
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-    id INT NOT NULL AUTO_INREMENT PRIMARY KEY,
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    name VARCHAR(255)
 );
